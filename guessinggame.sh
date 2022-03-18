f912de82190ae91cb8c605d1f56497cce07e3667
Jorge-Cepeda <101848630+Jorge-Cepeda@users.noreply.github.com>
2022-03-17 20:12:51 -0600

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,38 @@
+echo "Welcome to the Guessing Game from George~~"
+
+function numberOfFiles() {
+    
+    local files = $(ls -1 | wc -1)-1
+    
+}
+
+function GuessAgain() {
+
+   echo "Enter your Guess dear pleayer!"
+   read guess
+   
+}
+
+   CorrectFiles = $(numberOfFiles)
+
+function GuessGame() {
+   
+   while[[$guess -ne $CorrectFiles]]
+   do
+      
+     if [$number -lt $CorrectFiles]
+      then
+        echo "You are close but try with a higher number~~"
+     
+     elif [$number -gt $CorrectFile]
+      then
+        echo "You are close but try with a lower number~~"
+      else
+        echo "Congratulations you finally reach the correct answer!!"
+      break;
+        fi
+        GuessAgain
+      done  
+}
+
+GuessGame
